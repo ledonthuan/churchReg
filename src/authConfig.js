abbfbef12ad7cf7d99708205e8c72f55c3d0f893
Don-Thuan Le <ledonthuan@gmail.com>
2023-03-16 18:23:10 -0400

    simple update

---
+++ src/authConfig.js
@@ -0,0 +1,21 @@
+export const msalConfig = {
+    auth: {
+      clientId: "565e4287-0170-484f-ada9-7e5489ea14dd",
+      authority: "https://login.microsoftonline.com/24c19f77-b298-41f1-ae2f-b9f901e32da6", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+  
+  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+  export const loginRequest = {
+   scopes: ["User.Read"]
+  };
+  
+  // Add the endpoints here for Microsoft Graph API services you'd like to use.
+  export const graphConfig = {
+      graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  };
